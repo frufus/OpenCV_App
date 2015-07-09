fc8f7a9036d3e9d5a8cc2d0f521397d1cbd24007
Tabble <tanja_bluecher@yahoo.de>
2015-07-09 09:49:26 +0200

    Accelerometer in Play Activity eingebaut

--- app/src/main/java/com/frufus/opencv20/PlayActivity.java
+++ app/src/main/java/com/frufus/opencv20/PlayActivity.java
@@ -26,6 +26,10 @@ public class PlayActivity extends Activity {
     private float positionY;
 
     private  int RADIUS = 50;
+    private float[] gravity = new float[3];
+    private float[] linear_acceleration = new float[3];
+    private Double accelerationX;
+    private Double accelerationY;
 
 
     @Override
@@ -36,10 +40,61 @@ public class PlayActivity extends Activity {
         Intent getPlayButtonIntent = getIntent();
         filePathPicture = getPlayButtonIntent.getExtras().getString("File_Path");
 
+        initSensors();
 
 
 
 
+
+    }
+    private  void initSensors(){
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        SensorEventListener listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent e) {
+                handleSensorEvents(e);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+
+    }
+    private void handleSensorEvents(SensorEvent e){
+        if (e.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            onAccelerometerChanged(e);
+        }
+    }
+
+    private void onAccelerometerChanged(SensorEvent e){
+
+        // Hier kommt ein Hochpassfilter um die Erdbeschleunigung herauszurechnen
+        final float alpha = 0.8f;
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * e.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * e.values[1];
+
+
+        // Remove the gravity contribution with the high-pass filter.
+        linear_acceleration[0] = e.values[0] - gravity[0];
+        linear_acceleration[1] = e.values[1] - gravity[1];
+
+        Float accX =linear_acceleration[0];
+        Float accY = linear_acceleration[1];
+
+        accelerationX = accX.doubleValue();
+        accelerationY = accY.doubleValue();
+
+        String tag = "PlayActivity";
+        Log.d(tag,accelerationX.toString());
+        Log.d(tag, accelerationY.toString());
     }
 
     class DrawingView extends SurfaceView {
